a59a432513db7aa25d95dbba8ee56b93bad82c08
RITIK MAKHIJA <ritik-prog@users.noreply.github.com>
2020-08-02 17:43:27 +0530

    some effective changes and breakdown of code in essay term

--- 1. String Reverse (Client-Server) Python/server.py
+++ 1. String Reverse (Client-Server) Python/server.py
@@ -3,7 +3,7 @@ import socket
  
 # next create a socket object
 s = socket.socket()         
-print "Socket successfully created"
+print("Socket successfully created")
  
 # reserve a port on your computer in our
 # case it is 12345 but it can be anything
@@ -15,11 +15,11 @@ port = 12345
 # this makes the server listen to requests 
 # coming from other computers on the network
 s.bind(('', port))        
-print "socket binded to %s" %(port)
+print("socket binded to",port)
  
 # put the socket into listening mode
 s.listen(5)     
-print "socket is listening"           
+print("socket is listening")
  
 # a forever loop until we interrupt it or 
 # an error occurs
@@ -27,19 +27,19 @@ while True:
  
    # Establish connection with client.
    c, addr = s.accept()     
-   print 'Got connection from', addr
+   print('Got connection from', addr)
 
    # Get data from client
-   data=c.recv(1024)
+   data=c.recv(1024).decode()
    
    #Reverse data received from client
    data = data[::-1]
 
    if not data:
-     break
+      break
    
    # Send back reversed data to client   
-   c.sendall(data)
+   c.sendall(bytes(data,'utf-8'))
 
 
    # send a thank you message to the client. 
